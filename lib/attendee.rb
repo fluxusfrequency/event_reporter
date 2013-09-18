a5d745667adc5189ed266caa6cf758c95a05fba8
Ben Lewis <bennlewis@gmail.com>
2013-09-18 07:12:57 -0600

    added instance vars to attendee

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,6 +1,16 @@
 class Attendee
 
   def initialize (id, regdate, first_name, last_name, email_address, homephone, street, city, state, zipcode)
+    @id = id
+    @reg_date = regdate
+    @first_name = first_name
+    @last_name = last_name
+    @email = email_address
+    @phone = homephone
+    @street = street
+    @city = city
+    @state = state
+    @zipcode = zipcode
   end
 
 end
