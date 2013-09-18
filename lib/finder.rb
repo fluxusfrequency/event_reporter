055c41ec8548bd16edae05063c0975b482e5efe3
Ben Lewis <bennlewis@gmail.com>
2013-09-18 07:44:14 -0600

    Created printing_test branch after completing class divisions

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,19 +1,3 @@
 class Finder
 
-  def find_by_column(column)
-    if column.nil?
-      puts "Couldn't find any data matching the type #{column}."
-      return
-    end
-    add_to_queue(column)
-  end
-
-  def find_by_criteria(column, criteria)
-    if column.nil? || criteria.nil?
-      puts "Couldn't find any data matching the type #{column} or the criteria #{criteria}."
-      return
-    end
-    add_to_queue_with_criteria(column, criteria)
-  end
-
 end
