e4aa78fec8786a6e30d0173ae94fb040ce8bec5e
Ben Lewis <bennlewis@gmail.com>
2013-09-19 07:41:11 -0600

    added phone number cleaning

--- lib/event_reporter/attendee_list.rb
+++ lib/event_reporter/attendee_list.rb
@@ -8,6 +8,7 @@ class AttendeeList
   def initialize(file)
     @loader = Loader.new
     @zipper = Zipcode.new
+    @phoney = Phone.new
     @data = @loader.load_file(file)
     @list = []
     populate_list
@@ -21,7 +22,7 @@ class AttendeeList
         :first_name => row[:first_name],
         :last_name => row[:last_name],
         :email => row[:email_address],
-        :phone => row[:homephone],
+        :phone => @phoney.clean(row[:homephone]),
         :street => row[:street],
         :city => row[:city],
         :state => row[:state],
--- lib/event_reporter/phone_number.rb
+++ lib/event_reporter/phone_number.rb
@@ -1,21 +1,35 @@
-class PhoneNumber
-  attr_accessor :phone
+class Phone
 
-  def initialize(phone)
-    @phone = phone
-  end
-
-  def clean
-    if @phone
-      @phone = @phone.scan(/[0-9]/).join
-      if @phone.length == 11 && @phone.start_with?("1")
-        @phone = @phone[1..-1]
+  def clean(phone)
+    if phone.nil?
+      phone = "0000000000"
+    else
+      phone = phone.scan(/[0-9]/).join
+      if phone.length == 11 && phone.start_with?("1")
+        phone = phone[1..-1]
+      elsif phone.length != 10
+        phone = "0000000000"
+      else
+        return phone
       end
-      if @phone.length != 10
-        @phone = "0000000000"
-      end
-      return @phone
     end
   end
 
 end
+
+
+# class Zipcode
+
+#   def clean(zipcode)
+#     if zipcode.nil?
+#       zipcode = "00000"
+#     else
+#       zipcode = zipcode.scan(/[0-9]/).join
+#       if zipcode.length != 5
+#         zipcode = "00000"
+#       end
+#       return zipcode
+#     end
+#   end
+
+# end
