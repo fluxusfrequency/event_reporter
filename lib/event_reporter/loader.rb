5924cf7480b4ea41167a9d8af794b4fb3de9a80f
Ben Lewis <bennlewis@gmail.com>
2013-09-22 06:51:07 -0600

    refactored loader class

--- lib/event_reporter/loader.rb
+++ lib/event_reporter/loader.rb
@@ -2,14 +2,8 @@ require 'csv'
 
 class Loader
 
-  attr_reader :contents
-
-  def initialize
-    @contents = []
-  end
-
   def load_file(filename)
-    @contents = CSV.open(filename, headers: true, header_converters: :symbol)
+    CSV.open(filename, headers: true, header_converters: :symbol)
   end
 
 end
